f714d16cafdef78cfd5cc86d7209ce5cc26c1d64
shrushhh <shrushtiswarnakar95@gmail.com>
2023-12-12 12:50:30 +0530

    Changed the number

--- .vscode/Maps.cpp
+++ .vscode/Maps.cpp
@@ -4,7 +4,7 @@ using namespace std;;
 int main(){
     map<int,int> m;
     m.insert({1,3});  //key->1 and value->2
-    m[3] = 5;         //key->3 and value->5
+    m[3] = 6;         //key->3 and value->5
     m[4] = 8;
     m[2] = 20;
 
